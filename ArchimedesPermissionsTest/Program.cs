7fbc53c3f658759c9a7a32d2b9fb36409779dc3b
Michael Tanczos <mike@logic-gate.com>
2023-02-25 23:58:09 -0500

    Removing internal definitions

--- ArchimedesPermissionsTest/Program.cs
+++ ArchimedesPermissionsTest/Program.cs
@@ -1,6 +1,9 @@
 ﻿using ArchimedesPermissionsTest;
 using Microsoft.Extensions.Configuration;
 using System.Reflection;
+using Archimedes.Domain.Entities.Permission.Schema;
+using SpiceDb;
+using SpiceDb.Models;
 
 // This is just to keep the server address and token private
 var builder = new ConfigurationBuilder()
@@ -13,12 +16,16 @@ var secrets = configurationRoot.GetSection("AuthZed").Get<Secrets>();
 if (secrets is null)
     throw new ArgumentException("Invalid secrets configuration");
 
-var client = new SpiceDb.SpiceDbClient(secrets.ServerAddress, secrets.Token);
-var schema = Assembly.GetExecutingAssembly().ReadResourceAsync("archimedes-schema-001.zed").Result;
+var client = new SpiceDbClient(secrets.ServerAddress, secrets.Token, "archtest");
+var schema = typeof(ZedConfig).Assembly.ReadResourceAsync("archimedes-schema-001.zed").Result;
 
-client.ImportSchemaFromStringAsync(schema, "archtest").GetAwaiter().GetResult();
+client.DeleteRelationshipsAsync(new RelationshipFilter { Type = "group" }).Wait();
+client.DeleteRelationshipsAsync(new RelationshipFilter { Type = "organization" }).Wait();
+client.DeleteRelationshipsAsync(new RelationshipFilter { Type = "platform" }).Wait();
 
-var schema_read = client.ExportSchema();
+client.ImportSchemaFromStringAsync(schema).GetAwaiter().GetResult();
+
+var schema_read = client.ReadSchema();
 
 if (client.CheckPermission(ZedUser.WithId("abcd").GroupCanJoin(ZedGroup.WithId("abcd"))).HasPermission)
 {
